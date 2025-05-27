85b751f459eb373b7868cb30e0c4a136929c8358
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-05-27 07:06:00 -0800

    style:code format

--- game/src/index.py
+++ game/src/index.py
@@ -1,17 +1,17 @@
-import  pygame
+import pygame
 import sys
 
 from pygame.examples.grid import TITLE
 
 # Initialize pygame
 pygame.init()
-WIDTH =800
-HEIGHT =400
-TITLE ="Run Baby Run"
-MAX_FRAME_RATE =60
+WIDTH = 800
+HEIGHT = 400
+TITLE = "Run Baby Run"
+MAX_FRAME_RATE = 60
 
 # Display surface
-screen =pygame.display.set_mode((WIDTH,HEIGHT))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption(TITLE)
 clock = pygame.time.Clock()
 
@@ -21,7 +21,7 @@ while True:
             pygame.quit()
             sys.exit()
 
-    #Draw all our objects
-    #update everything
+    # Draw all our objects
+    # update everything
     pygame.display.update()
     clock.tick(MAX_FRAME_RATE)
