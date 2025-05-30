aee9021532d7818921fd61adec71eae1a60dc3ab
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-05-30 02:08:32 -0800

    feat: Adding the score related logic and displaying it on game over scenario.

--- game/src/index.py
+++ game/src/index.py
@@ -14,6 +14,8 @@ SLIME_OFFSET = 55
 font_path = "../assets/font/04B_30__.TTF"
 slime_one_path = "../assets/Slime/slime1_left_walk.png"
 test_font = pygame.font.Font(font_path, 50)
+font_size_30 = pygame.font.Font(font_path, 30)
+font_size_15 = pygame.font.Font(font_path, 15)
 
 # Display surface
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -32,8 +34,10 @@ print(user_surface_scaled.get_height(),user_surface_scaled.get_width())
 
 intro_surface = pygame.image.load("../assets/intro/intro_grafield_dance.png").convert_alpha()
 intro_surface_scaled =pygame.transform.scale(intro_surface,(intro_surface.get_width()*3,intro_surface.get_height()*3))
-intro_surface_scaled_rect = intro_surface_scaled.get_rect(center =(WIDTH/2,HEIGHT/2))
+intro_surface_scaled_rect = intro_surface_scaled.get_rect(center =(WIDTH/2,(HEIGHT/2)+100))
 
+
+has_started = False
 user_gravity = 0
 game_active = False
 
@@ -41,14 +45,31 @@ pygame.display.set_caption(TITLE)
 clock = pygame.time.Clock()
 test_surface = test_font.render(TITLE, True, "dark blue")
 test_surface_rect = test_surface.get_rect(center=(WIDTH / 2, 250))
-start_time_milli = 0
+test_surface_intro_rect = test_surface.get_rect(center=(WIDTH / 2, 100))
 
+start_time_milli = 0
+score =0
+
+to_start = "Press SPACE to start"
+to_start_font = font_size_30.render(to_start, True, "White")
+to_start_font_rect = to_start_font.get_rect(center=(WIDTH / 2, 600))
+
+instruction= " Press SPACE to jump over slimes"
+instruction_font =font_size_15.render(instruction, True, "grey")
+instruction_font_rect = instruction_font.get_rect(center=(WIDTH / 2, 650))
+
+def get_score():
+    score_info = f"Score: {score}"
+    score_info_font = test_font.render(score_info, True, "White")
+    score_info_font_rect = score_info_font.get_rect(center=(WIDTH / 2, 250))
+    screen.blit(score_info_font, score_info_font_rect)
 
 def get_timer_surface():
     current_time = int((pygame.time.get_ticks() - start_time_milli) / 1000)
     timer_surface = test_font.render(f"{current_time}", True, "red")
     timer_surface_rec = timer_surface.get_rect(center=(WIDTH / 2, 100))
     screen.blit(timer_surface, timer_surface_rec)
+    return current_time
 
 while True:
     for event in pygame.event.get():
@@ -62,6 +83,7 @@ while True:
             else:
                 if event.key == pygame.K_SPACE:
                     game_active = True
+                    has_started = True
                     start_time_milli = pygame.time.get_ticks()
 
     if game_active:
@@ -73,7 +95,7 @@ while True:
         screen.blit(test_surface, test_surface_rect)
         screen.blit(slime_surface_scaled, slime_surface_rect)
         pygame.draw.rect(screen,"red",slime_surface_rect,20,30)
-        get_timer_surface()
+        score =get_timer_surface()
 
         # slime_x = slime_x - 5
         slime_surface_rect.left -= 1
@@ -99,8 +121,12 @@ while True:
 
     else:
         screen.fill((3, 84, 84))
+        screen.blit(test_surface, test_surface_intro_rect)
         screen.blit(intro_surface_scaled,intro_surface_scaled_rect)
-        print("GAME OVER")
+        if has_started:
+           get_score()
+        screen.blit(to_start_font, to_start_font_rect)
+        screen.blit(instruction_font, instruction_font_rect)
 
     pygame.display.update()
     clock.tick(MAX_FRAME_RATE)
