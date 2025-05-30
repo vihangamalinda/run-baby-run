ae0828edf739a1ace3afc514e9f0bf546a562fb4
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-05-30 00:48:13 -0800

    feat: Scaling the user and slime.

--- game/src/index.py
+++ game/src/index.py
@@ -8,6 +8,8 @@ HEIGHT = 720
 TITLE = "Run Baby Run"
 MAX_FRAME_RATE = 60
 GROUND_LEVEL = 665
+USER_OFFSET =30
+SLIME_OFFSET = 55
 
 font_path = "../assets/font/04B_30__.TTF"
 slime_one_path = "../assets/Slime/slime1_left_walk.png"
@@ -19,9 +21,15 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 sky_surface = pygame.image.load('../assets/sky.png').convert()
 ground_surface = pygame.image.load("../assets/ground.png").convert()
 slime_surface = pygame.image.load(slime_one_path).convert_alpha()
-slime_surface_rect = slime_surface.get_rect(midbottom=(WIDTH, GROUND_LEVEL))
+slime_surface_scaled  =pygame.transform.scale(slime_surface,(slime_surface.get_width()*3,slime_surface.get_width()*3))
+slime_surface_rect = slime_surface_scaled.get_rect(midbottom=(WIDTH, GROUND_LEVEL + SLIME_OFFSET))
+
 user_surface = pygame.image.load("../assets/user/user_walk.png").convert_alpha()
-user_surface_rect = user_surface.get_rect(midbottom=(50, GROUND_LEVEL))
+print(user_surface.get_height(),user_surface.get_width())
+user_surface_scaled = pygame.transform.scale(user_surface,(user_surface.get_width()*3,user_surface.get_height()*3))
+user_surface_rect = user_surface_scaled.get_rect(midbottom=(50, GROUND_LEVEL+USER_OFFSET))
+print(user_surface_scaled.get_height(),user_surface_scaled.get_width())
+
 user_gravity = 0
 game_active = True
 
@@ -45,7 +53,7 @@ while True:
             sys.exit()
         if event.type == pygame.KEYDOWN:
             if game_active:
-                if event.key == pygame.K_SPACE and user_surface_rect.bottom == GROUND_LEVEL:
+                if event.key == pygame.K_SPACE and user_surface_rect.bottom == (GROUND_LEVEL+USER_OFFSET):
                     user_gravity = -30
             else:
                 if event.key == pygame.K_SPACE:
@@ -57,8 +65,11 @@ while True:
         screen.blit(ground_surface, (0, 420))
         pygame.draw.rect(screen, "yellow", test_surface_rect)
         pygame.draw.rect(screen, "yellow", test_surface_rect, 20, 30)
+        pygame.draw.rect(screen, "yellow", user_surface_rect, 20, 30)
         screen.blit(test_surface, test_surface_rect)
         screen.blit(slime_surface, slime_surface_rect)
+        screen.blit(slime_surface_scaled, slime_surface_rect)
+        pygame.draw.rect(screen,"red",slime_surface_rect,20,30)
         get_timer_surface()
 
         # slime_x = slime_x - 5
@@ -76,11 +87,11 @@ while True:
         user_gravity += 1
         user_surface_rect.y += user_gravity
 
-        if user_surface_rect.bottom > GROUND_LEVEL:
-            user_surface_rect.bottom = GROUND_LEVEL
+        if user_surface_rect.bottom > GROUND_LEVEL +USER_OFFSET:
+            user_surface_rect.bottom = GROUND_LEVEL+USER_OFFSET
 
-        screen.blit(user_surface, user_surface_rect)
-        if slime_surface_rect.collidepoint(user_surface_rect.midleft):
+        screen.blit(user_surface_scaled, user_surface_rect)
+        if slime_surface_rect.collidepoint(user_surface_rect.midbottom):
             game_active = False
 
     else:
